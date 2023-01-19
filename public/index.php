da7aff83383a90d493b956b10df72ffe36256347
KauaVicto <kauavictor259@gmail.com>
2023-01-18 22:37:26 -0300

    .

--- public/index.php
+++ public/index.php
@@ -3,7 +3,9 @@ session_start();
 
 require_once "../vendor/autoload.php";
 
+/* Doctrine */
 $dotenv = Dotenv\Dotenv::createUnsafeImmutable(__DIR__.'/../');
 $dotenv->load();
 
+/* Slim */
 (require __DIR__ . '/../config/bootstrap.php')->run();
