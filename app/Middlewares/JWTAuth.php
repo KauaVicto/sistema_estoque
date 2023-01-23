aaab9623d4c08e045f6e61d4e7a7c22261122001
Kaua Victor <kauavictor259@gmail.com>
2023-01-23 10:34:18 -0300

    removido uses

--- app/Middlewares/JWTAuth.php
+++ app/Middlewares/JWTAuth.php
@@ -2,11 +2,9 @@
 
 namespace App\Middlewares;
 
-use DateTime;
 use Nyholm\Psr7\Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Psr\Http\Server\RequestHandlerInterface as RequestHandler;
-use Tuupola\Middleware\JwtAuthentication;
 use Firebase\JWT\JWT;
 
 class JWTAuth extends \App\Controller\Controller
